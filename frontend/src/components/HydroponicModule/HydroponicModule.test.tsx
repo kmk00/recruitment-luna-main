3f72ccef5ddec3bf007bed842372c14a0cf9ed93
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-07-08 23:34:31 +0200

    Add tests

--- frontend/src/components/HydroponicModule/HydroponicModule.test.tsx
+++ frontend/src/components/HydroponicModule/HydroponicModule.test.tsx
@@ -36,4 +36,51 @@ describe("HydroponicModule", () => {
       "/3f21t-4f4f-6f6f-8f8f-9f9f9f9f9f9f"
     );
   });
+
+  test("It should render the target temperature correctly", () => {
+    render(
+      <MemoryRouter>
+        <HydroponicModule
+          name="Module 1"
+          id="1"
+          targetTemperature={20}
+          available={true}
+        />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText(/20°c/i)).toBeDefined();
+  });
+
+  test("It should say that the module is not available when available is false", () => {
+    render(
+      <MemoryRouter>
+        <HydroponicModule
+          name="Module 1"
+          id="1"
+          targetTemperature={20}
+          available={false}
+        />
+      </MemoryRouter>
+    );
+
+    const statusElement = screen.getAllByText(/not available/i);
+    expect(statusElement).toBeDefined();
+    expect(statusElement.length).toBe(2);
+  });
+
+  test("It should say that the module is available when available is true", () => {
+    render(
+      <MemoryRouter>
+        <HydroponicModule
+          name="Module 1"
+          id="1"
+          targetTemperature={20}
+          available={true}
+        />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText(/available/i)).toBeDefined();
+  });
 });
