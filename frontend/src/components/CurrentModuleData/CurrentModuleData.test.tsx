a53e49fad8afcc0e7070232214ea2fd758f70317
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-07-09 17:32:50 +0200

    Create CurrentModuleData.test.tsx

---
+++ frontend/src/components/CurrentModuleData/CurrentModuleData.test.tsx
@@ -0,0 +1,11 @@
+describe("CurrentModuleData", () => {
+  test("It should have the edit button disabled when the module is not available", () => {});
+
+  test("It should have the edit button enabled when the module is available", () => {});
+
+  test("It should display the edit modal after clicking the edit button", () => {});
+
+  test("It should have go back button", () => {});
+
+  test("It should render the module details", () => {});
+});
